21731fff2d2693f5767ffe6570e2ddd217fcc6bc
futurespacefreight <future.space.freight@gmail.com>
2026-08-19 14:48:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("HELIX Terminal gestartet.");
+function updateClock(){
+
+    const now = new Date();
+
+    document.getElementById("currentTime").textContent =
+        now.toLocaleTimeString("de-DE");
+
+    document.getElementById("currentDate").textContent =
+        now.toLocaleDateString("de-DE");
+
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
