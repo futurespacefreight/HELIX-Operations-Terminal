75c078a745387552adf633628da321f28c337c32
futurespacefreight <future.space.freight@gmail.com>
2026-08-20 11:31:54 +0200

    script.js aktualisieren

--- script.js
+++ script.js
@@ -226,3 +226,29 @@ refreshInterface();
 showNotification("ARES • Schiffsdaten aktualisiert");
 
 }
+
+function showNotification(text){
+
+const note=document.createElement("div");
+
+note.textContent=text;
+
+note.style.position="fixed";
+note.style.right="20px";
+note.style.bottom="20px";
+note.style.padding="15px";
+note.style.background="#183042";
+note.style.color="white";
+note.style.border="1px solid #7ec9ff";
+note.style.borderRadius="8px";
+note.style.zIndex="9999";
+
+document.body.appendChild(note);
+
+setTimeout(()=>{
+
+note.remove();
+
+},2500);
+
+}
