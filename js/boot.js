38dd54f7617f06cacce78ece90a4055a1b475b8e
futurespacefreight <future.space.freight@gmail.com>
2026-08-20 13:24:23 +0200

    boot.js aktualisieren

--- js/boot.js
+++ js/boot.js
@@ -1,1 +1,45 @@
-fg
+/* ======================================================
+   BOOT SEQUENCE
+====================================================== */
+
+const bootMessages = [
+
+    "Initializing Core...",
+    "Loading Navigation...",
+    "Loading DataCore...",
+    "Connecting ARES...",
+    "Loading Crew Database...",
+    "Loading Mission Control...",
+    "Synchronizing Station Link...",
+    "System Online"
+
+];
+
+function startBootSequence(){
+
+    let bootIndex = 0;
+
+    const bootInterval = setInterval(()=>{
+
+        document.getElementById("bootStatus").textContent =
+            bootMessages[bootIndex];
+
+        bootIndex++;
+
+        if(bootIndex >= bootMessages.length){
+
+            clearInterval(bootInterval);
+
+            setTimeout(()=>{
+
+                document.getElementById("bootScreen").style.display = "none";
+
+                document.getElementById("app").style.display = "flex";
+
+            },700);
+
+        }
+
+    },400);
+
+}
