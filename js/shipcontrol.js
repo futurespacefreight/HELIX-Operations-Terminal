2692a6e04b5b96fce42161b6136414927ace341b
futurespacefreight <future.space.freight@gmail.com>
2026-08-20 13:19:16 +0200

    shipcontrol.js aktualisieren

--- js/shipcontrol.js
+++ js/shipcontrol.js
@@ -4,12 +4,30 @@
 
 function loadShipControl(){
 
-    ...
+    document.getElementById("inputLocation").value =
+    DataCore.ship.location;
+
+    document.getElementById("inputArea").value =
+    DataCore.ship.area;
+
+    document.getElementById("inputDocked").checked =
+    DataCore.ship.docked;
 
 }
 
 function saveShipControl(){
 
-    ...
+    DataCore.ship.location =
+    document.getElementById("inputLocation").value;
+
+    DataCore.ship.area =
+    document.getElementById("inputArea").value;
+
+    DataCore.ship.docked =
+    document.getElementById("inputDocked").checked;
+
+    refreshInterface();
+
+    showNotification("ARES • Schiffsdaten aktualisiert");
 
 }
