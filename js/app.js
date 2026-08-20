164151c96328066fab2b93e38ecf3962fbe4a25b
futurespacefreight <future.space.freight@gmail.com>
2026-08-20 13:19:55 +0200

    app.js aktualisieren

--- js/app.js
+++ js/app.js
@@ -1,6 +1,48 @@
 /* ======================================================
-   APP START
+   HELIX APP
 ====================================================== */
+
+const bootMessages = [
+
+"Initializing Core...",
+"Loading Navigation...",
+"Loading DataCore...",
+"Connecting ARES...",
+"Loading Crew Database...",
+"Loading Mission Control...",
+"Synchronizing Station Link...",
+"System Online"
+
+];
+
+let bootIndex = 0;
+
+document.getElementById("app").style.display="none";
+
+const bootInterval = setInterval(()=>{
+
+    document.getElementById("bootStatus").textContent =
+    bootMessages[bootIndex];
+
+    bootIndex++;
+
+    if(bootIndex>=bootMessages.length){
+
+        clearInterval(bootInterval);
+
+        setTimeout(()=>{
+
+            document.getElementById("bootScreen").style.display="none";
+
+            document.getElementById("app").style.display="flex";
+
+        },700);
+
+    }
+
+},400);
+
+/* ==================================== */
 
 loadData();
 
