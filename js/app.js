c3e0b7314d0b3e13b7e0fff0f9a28424d35c3021
futurespacefreight <future.space.freight@gmail.com>
2026-08-20 13:40:15 +0200

    app.js aktualisieren

--- js/app.js
+++ js/app.js
@@ -4,19 +4,31 @@
 
 function initializeApplication(){
 
-    console.log("HELIX OS Initialisierung...");
+    console.log("1");
 
     document.getElementById("app").style.display = "none";
 
+    console.log("2");
+
     loadData();
+
+    console.log("3");
 
     updateClock();
 
+    console.log("4");
+
     setInterval(updateClock,1000);
+
+    console.log("5");
 
     updateDashboard();
 
+    console.log("6");
+
     startBootSequence();
+
+    console.log("7");
 
 }
 
