c73321ff824fe7b88ff204ddc22afb3affa8382b
futurespacefreight <future.space.freight@gmail.com>
2026-08-20 19:48:53 +0200

    dashboard.js aktualisieren

--- js/dashboard.js
+++ js/dashboard.js
@@ -9,6 +9,43 @@ function updateDashboard() {
 
     const locationElement = document.getElementById("location");
 
+/* ======================================================
+   ARES STATUS
+====================================================== */
+
+document.getElementById("statusText").textContent =
+"ONLINE";
+
+document.getElementById("aresStatus").textContent =
+"● ONLINE";
+
+document.getElementById("commStatus").textContent =
+"● NOMINAL";
+
+document.getElementById("lifeSupportStatus").textContent =
+"● STABIL";
+
+document.getElementById("reactorStatus").textContent =
+"● 100 %";
+
+if(DataCore.ship.docked){
+
+    document.getElementById("dockStatus").textContent =
+    "● ANGEDOCKT";
+
+    document.getElementById("dockStatus").className =
+    "status-green";
+
+}else{
+
+    document.getElementById("dockStatus").textContent =
+    "● IM FLUG";
+
+    document.getElementById("dockStatus").className =
+    "status-yellow";
+
+}
+
     if(DataCore.ship.area){
 
         locationElement.textContent =
