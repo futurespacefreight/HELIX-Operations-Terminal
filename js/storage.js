b4b7fd4a5d72b5e2c2571fced8cc4b96f36c3ebd
futurespacefreight <future.space.freight@gmail.com>
2026-08-20 13:18:06 +0200

    storage.js aktualisieren

--- js/storage.js
+++ js/storage.js
@@ -2,14 +2,37 @@
    LOCAL STORAGE
 ====================================================== */
 
-function saveData(){
+function saveData() {
 
-    ...
+    localStorage.setItem(
+        "HELIX_DataCore",
+        JSON.stringify(DataCore)
+    );
 
 }
 
-function loadData(){
+function loadData() {
 
-    ...
+    const savedData = localStorage.getItem("HELIX_DataCore");
+
+    if (!savedData) {
+
+        console.log("Kein Spielstand gefunden. Neuer DataCore wird erstellt.");
+
+        saveData();
+
+        return;
+
+    }
+
+    const loaded = JSON.parse(savedData);
+
+    Object.assign(DataCore.ship, loaded.ship);
+    Object.assign(DataCore.mission, loaded.mission);
+    Object.assign(DataCore.finance, loaded.finance);
+    Object.assign(DataCore.lifeSupport, loaded.lifeSupport);
+    Object.assign(DataCore.crew, loaded.crew);
+
+    console.log("Spielstand erfolgreich geladen.");
 
 }
