875f6ed734aab543b0328baeeb48f35f93d10eb7
futurespacefreight <future.space.freight@gmail.com>
2026-08-20 13:18:50 +0200

    dashboard.js aktualisieren

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,9 +2,29 @@
    DASHBOARD
 ====================================================== */
 
-function updateDashboard(){
+function updateDashboard() {
 
-    ...
+    document.getElementById("credits").textContent =
+        DataCore.finance.credits.toLocaleString("de-DE") + " CR";
+
+    const locationElement = document.getElementById("location");
+
+    if(DataCore.ship.area){
+
+        locationElement.textContent =
+        DataCore.ship.location + " • " + DataCore.ship.area;
+
+    }else{
+
+        locationElement.textContent =
+        DataCore.ship.location;
+
+    }
+
+    document.getElementById("mission").textContent =
+        DataCore.mission.title;
+
+    saveData();
 
 }
 
